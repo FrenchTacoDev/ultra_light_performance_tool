daa95462c8f816c2d72a7c3e73fb4c220761baeb
WorldpixelSoftware <frenchtacodev@gmail.com>
2023-11-22 15:09:18 +0100

    Update main.dart

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -7,6 +7,7 @@ import 'package:ultra_light_performance_tool/ulpt.dart';
 
 void main() {
 
+  //This is needed to setup SQLite FFI so the database library can work on desktop!
   if(io.Platform.isWindows ||io.Platform.isMacOS){
     sqfliteFfiInit();
     databaseFactory = databaseFactoryFfi;
