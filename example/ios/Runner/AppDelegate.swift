adc9fc18a89e895a086d45ec386cb29c67a94585
WorldpixelSoftware <frenchtacodev@gmail.com>
2023-12-27 19:15:30 +0100

    File open support iOS
     - Implemented file format for .ulpt files in the example info.plist
    
    - Implemented native swift function thats called when app opens with url

--- example/ios/Runner/AppDelegate.swift
+++ example/ios/Runner/AppDelegate.swift
@@ -3,11 +3,29 @@ import Flutter
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+    
+    private var commChannel: FlutterMethodChannel? = nil;
+    
+    override func application(
+      _ application: UIApplication,
+      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        GeneratedPluginRegistrant.register(with: self)
+        let controller: FlutterViewController = window?.rootViewController as! FlutterViewController;
+        commChannel = FlutterMethodChannel(name: "nativeCommChannel", binaryMessenger: controller.binaryMessenger);
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
+    
+    override func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        
+        if(commChannel == nil){return true;}
+        
+        if #available(iOS 16, *){
+            commChannel!.invokeMethod("onArgsFromNative", arguments: url.path());
+        }else{
+            commChannel!.invokeMethod("onArgsFromNative", arguments: url.absoluteString);
+        }
+        
+        return true;
+    }
 }
