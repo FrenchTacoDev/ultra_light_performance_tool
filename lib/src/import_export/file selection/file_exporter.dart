613b33f1496ac5b041b293cbfaf062ec60862c74
WorldpixelSoftware <frenchtacodev@gmail.com>
2024-03-13 02:44:33 +0100

    Squashed commit of the following:
    commit 2b1f61ad0c81aed96d6e5e86dbfebce325d9ce4b
    Author: WorldpixelSoftware <frenchtacodev@gmail.com>
    Date:   Wed Mar 13 02:34:05 2024 +0100
    
        Update themes.dart
    
        Added different textthemes for the ulpt theme
    
    commit 27d45e3f8fdf0cecc7776182f80b5ba93389ca92
    Author: WorldpixelSoftware <frenchtacodev@gmail.com>
    Date:   Wed Mar 13 01:52:23 2024 +0100
    
        Android screen adjustment
    
         - Panelheight for PerfCalcPanel adjusted on android to meet iOS sizes
         - Fixed calc button having to less space on medium sized layout
         - takeoff details rounded/floored headwind component
    
    commit 5f27b4b739200ed686609d93f54bd2d7606379f8
    Author: FrenchTacoDev <frenchtacodev@gmail.com>
    Date:   Mon Mar 11 10:21:29 2024 +0100
    
        Android File import/export
    
        Added ability to import/export files on android. Also added open file method by clicking on it.
    
    commit c1b62bc49f72545b1ed8474b745adaa5a4b11e20
    Author: WorldpixelSoftware <frenchtacodev@gmail.com>
    Date:   Thu Mar 7 20:09:34 2024 +0100
    
        file saving android
    
    commit c6cc99fd54f7eaadb23976a1eac8ca37e7ee187a
    Author: WorldpixelSoftware <frenchtacodev@gmail.com>
    Date:   Wed Mar 6 17:25:14 2024 +0100
    
        Android Rebuild
    
        rebcreated the standard android structure to be up to date

--- lib/src/import_export/file selection/file_exporter.dart
+++ lib/src/import_export/file selection/file_exporter.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:share_plus/share_plus.dart';
+import 'package:shared_storage/shared_storage.dart' as saf;
 import 'package:ultra_light_performance_tool/src/core/core.dart';
 
 ///Class for writing data to a user selected file(path).
@@ -13,6 +14,7 @@ class ULPTFileExporter{
   ///If [dict] is provided, will localize the export message for the user.
   Future<void> exportULPTData({required Uint8List data, required Size screenSize, Dictionary? dict}) async{
     if(Platform.isWindows || Platform.isMacOS) return _DesktopExporter().exportULPTData(data: data, screenSize: screenSize);
+    if(Platform.isAndroid) return _AndroidExplorer().exportULPTData(data: data, screenSize: screenSize);
     var filePath = join((await getTemporaryDirectory()).path, "PerfData.ulpt");
 
     var file = File(filePath);
@@ -25,6 +27,34 @@ class ULPTFileExporter{
       [XFile(filePath)],
       sharePositionOrigin: Rect.fromLTRB(0, 0, screenSize.width, 16),
     );
+  }
+}
+
+///Subclass used on Android. Is private bc should only be selected by the main function
+class _AndroidExplorer extends ULPTFileExporter{
+  @override
+  Future<void> exportULPTData({required Uint8List data,  required Size screenSize, Dictionary? dict}) async{
+
+    //Use cached file in share sheet so user can directly share.
+    var fileName = "PerfData.ulpt";
+
+    var cacheFile = File(join((await getTemporaryDirectory()).path, fileName));
+    if(await cacheFile.exists()) await cacheFile.delete(recursive: true);
+    await cacheFile.create(recursive: true);
+    await cacheFile.writeAsBytes(data);
+
+    var uri = await saf.openDocumentTree();
+
+    if(uri != null){
+      await saf.createFileAsBytes(uri, mimeType: "application/octet-stream", displayName: fileName, bytes: data);
+    }
+
+    await Share.shareXFiles(
+      [XFile(cacheFile.path)],
+      sharePositionOrigin: Rect.fromLTRB(0, 0, screenSize.width, 16),
+    );
+
+    await cacheFile.delete(recursive: true);
   }
 }
 
